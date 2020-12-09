3fdb0b85926195e539448fb0d293b2f6508e347d
Thibaut_Guerin <thibaut.guerin@edu.devinci.fr>
2020-12-09 12:01:58 +0100

    Setup AI controller

---
+++ Assets/Teams/TeamClaws/BOTController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DoNotModify;
+
+namespace TeamClaws
+{
+
+	public class BOTController : BaseSpaceShipController
+	{
+		private Blackboard _blackboard;
+
+		public override void Initialize(SpaceShip spaceship, GameData data)
+		{
+			_blackboard = GetComponent<Blackboard>();
+			_blackboard.Initialize(spaceship, data);
+		}
+
+		public override InputData UpdateInput(SpaceShip spaceship, GameData data)
+		{
+			float thrust = 1.0f;
+			float targetOrient = spaceship.Orientation + 90.0f;
+
+			_blackboard.UpdateData(data);
+
+			return new InputData(thrust, targetOrient, _blackboard.TriggerShoot, false, false);
+		}
+	}
+
+
+}
---
+++ Assets/Teams/TeamClaws/Tasks/Shoot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+
+namespace TeamClaws
+{
+    public class Shoot : Action
+    {
+        public override TaskStatus OnUpdate()
+        {
+            return TaskStatus.Success;
+        }
+    }
+}
