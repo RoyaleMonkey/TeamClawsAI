9d36dfa60f1c3d391674af2420227ce7fdd13f9b
Théo <theo.boldireff@edu.devinci.fr>
2020-12-10 18:21:17 +0100

    Fix déplacement (bloque toujours dans certains changements de direction)

--- Assets/Teams/TeamClaws/Tasks/GoToNextPoint.cs
+++ Assets/Teams/TeamClaws/Tasks/GoToNextPoint.cs
@@ -7,19 +7,33 @@ namespace TeamClaws
 {
     public class GoToNextPoint : Action
     {
+        //references
         public SharedTransform wayPointTransform;
         private Blackboard _blackboard;
         private SpaceShip spaceShip;
+
+        //dodge variables
+        private Vector2 minY;
+        private Vector2 maxY;
+        private Vector2 minX;
+        private Vector2 maxX;
+
+        private bool goUp;
+        private bool goDown;
+        private bool goLeft;
+        private bool goRight;
 
         private float newOrient;
         private bool asteroidDetected;
         private bool distanceCheck;
         public override TaskStatus OnUpdate()
         {
+            //references
             ref InputData inputData = ref GetComponent<BOTController>().inputData;
             _blackboard = GetComponent<BOTController>()._blackboard;
             spaceShip = _blackboard._latestGameData.SpaceShips[_blackboard._owner];
 
+            //raycast
             RaycastHit2D[] hit2D = Physics2D.LinecastAll(spaceShip.transform.position, spaceShip.transform.position + spaceShip.transform.right * 3);
             Debug.DrawLine(spaceShip.transform.position, spaceShip.transform.position + spaceShip.transform.right * 3);
 
@@ -32,24 +46,55 @@ namespace TeamClaws
                     if (hit.collider.CompareTag("Asteroid"))
                     {
                         //asteroid detected
-                        Vector2 min = new Vector2(hit.collider.bounds.center.x, hit.collider.bounds.min.y);
-                        Vector2 max = new Vector2(hit.collider.bounds.center.x, hit.collider.bounds.max.y);
 
-                        Debug.Log("min " + Vector2.Distance(min, spaceShip.Position));
-                        Debug.Log("max " + Vector2.Distance(max, spaceShip.Position));
+                        //get collider bounds
+                        minY = new Vector2(hit.collider.bounds.center.x, hit.collider.bounds.min.y);
+                        maxY = new Vector2(hit.collider.bounds.center.x, hit.collider.bounds.max.y);
+
+                        minX = new Vector2(hit.collider.bounds.min.x, hit.collider.bounds.center.y);
+                        maxX = new Vector2(hit.collider.bounds.max.x, hit.collider.bounds.center.y);
 
                         inputData.thrust = 0;
                         asteroidDetected = true;
 
-                        if(!distanceCheck && Vector2.Distance(min, spaceShip.Position) < Vector2.Distance(max, spaceShip.Position))
+                        if (spaceShip.Position.y < minY.y || spaceShip.Position.y > maxY.y)
                         {
-                            newOrient = -90;
-                            distanceCheck = true;
+                            if (!distanceCheck && Vector2.Distance(minX, spaceShip.Position) < Vector2.Distance(maxX, spaceShip.Position))
+                            {
+                                //dodge toward left bound
+                                Debug.Log("gauche");
+                                newOrient = (Mathf.Atan2(minX.y - spaceShip.Position.y, minX.x - spaceShip.Position.x) * 180 / Mathf.PI) + 45;
+                                goLeft = true;
+                                distanceCheck = true;
+                            }
+                            else if (!distanceCheck && Vector2.Distance(minX, spaceShip.Position) > Vector2.Distance(maxX, spaceShip.Position))
+                            {
+                                //dodge toward right bound
+                                Debug.Log("droite");
+                                newOrient = (Mathf.Atan2(maxX.y - spaceShip.Position.y, maxX.x - spaceShip.Position.x) * 180 / Mathf.PI) - 45;
+                                goRight = true;
+                                distanceCheck = true;
+                            }
                         }
-                        else if(!distanceCheck && Vector2.Distance(min, spaceShip.Position) > Vector2.Distance(max, spaceShip.Position))
+
+                        if (spaceShip.Position.x < minX.x || spaceShip.Position.x > maxX.x)
                         {
-                            newOrient = 90;
-                            distanceCheck = true;
+                            if (!distanceCheck && Vector2.Distance(minY, spaceShip.Position) < Vector2.Distance(maxY, spaceShip.Position))
+                            {
+                                // dodge toward down bound
+                                Debug.Log("bas");
+                                newOrient = (Mathf.Atan2(minY.y - spaceShip.Position.y, minY.x - spaceShip.Position.x) * 180 / Mathf.PI) + 45;
+                                goDown = true;
+                                distanceCheck = true;
+                            }
+                            else if (!distanceCheck && Vector2.Distance(minY, spaceShip.Position) > Vector2.Distance(maxY, spaceShip.Position))
+                            {
+                                //dodge toward up bound
+                                Debug.Log("haut");
+                                newOrient = (Mathf.Atan2(maxY.y - spaceShip.Position.y, maxY.x - spaceShip.Position.x) * 180 / Mathf.PI) - 45;
+                                goUp = true;
+                                distanceCheck = true;
+                            }
                         }
 
                         inputData.targetOrientation = newOrient;
@@ -59,18 +104,55 @@ namespace TeamClaws
 
             if (asteroidDetected)
             {
-                if (spaceShip.GetComponent<Rigidbody2D>().rotation == newOrient)
+                if (distanceCheck && Mathf.Approximately(spaceShip.GetComponent<Rigidbody2D>().rotation, newOrient))
                 {
                     //dodge asteroid
-                    asteroidDetected = false;
-                    distanceCheck = false;
+                    inputData.thrust = 1;
+
+                    if (goUp)
+                    {
+                        if (Mathf.Abs(spaceShip.Position.y) > Mathf.Abs(maxY.y))
+                        {
+                            goUp = false;
+                            asteroidDetected = false;
+                            distanceCheck = false;
+                        }
+                    }
+                    if (goDown)
+                    {
+                        if (Mathf.Abs(spaceShip.Position.y) < Mathf.Abs(minY.y))
+                        {
+                            goDown = false;
+                            asteroidDetected = false;
+                            distanceCheck = false;
+                        }
+                    }
+                    if (goLeft)
+                    {
+                        if (Mathf.Abs(spaceShip.Position.x) < Mathf.Abs(minX.x))
+                        {
+                            goLeft = false;
+                            asteroidDetected = false;
+                            distanceCheck = false;
+                        }
+                    }
+                    if (goRight)
+                    {
+                        if (Mathf.Abs(spaceShip.Position.x) > Mathf.Abs(maxX.x))
+                        {
+                            goRight = false;
+                            asteroidDetected = false;
+                            distanceCheck = false;
+                        }
+                    }
                 }
             }
 
-            if (!asteroidDetected)
+            else
             {
                 //go to position
-                inputData.targetOrientation = Mathf.Atan2(wayPointTransform.Value.position.y - spaceShip.Position.y, wayPointTransform.Value.position.x - spaceShip.Position.x) * 180 / Mathf.PI;
+                newOrient = Mathf.Atan2(wayPointTransform.Value.position.y - spaceShip.Position.y, wayPointTransform.Value.position.x - spaceShip.Position.x) * 180 / Mathf.PI;
+                inputData.targetOrientation = newOrient;
                 inputData.thrust = 1;
             }
 
